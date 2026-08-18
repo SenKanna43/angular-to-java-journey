acf8a69ac7bc2fbd7f2982ef1a3b34d50990607f
SenKanna43 <senthamaraikannanp@gmail.com>
2026-08-18 10:45:31 +0530

    Separated the class and main method

--- Day 02/Employee.java
+++ Day 02/Employee.java
@@ -2,8 +2,4 @@ class Employee {
     int id = 1;
     String name = "John Doe";
     String email = "john.doe@example.com";
-    public static void main(String[] args) {
-        Employee e = new Employee();
-        System.out.println(e.name);
-    }
 }
---
+++ Day 02/EmployeeDemo.java
@@ -0,0 +1,6 @@
+public class EmployeeDemo {
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        System.out.println(e.name);
+    }
+}
