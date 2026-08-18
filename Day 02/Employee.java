1495190f9c8230ed7b6ac2c904dbe82b7b33923e
SenKanna43 <senthamaraikannanp@gmail.com>
2026-08-18 10:35:37 +0530

    Created class & objects

---
+++ Day 02/Employee.java
@@ -0,0 +1,9 @@
+class Employee {
+    int id = 1;
+    String name = "John Doe";
+    String email = "john.doe@example.com";
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        System.out.println(e.name);
+    }
+}
